c3edd50d073eb1306547a4ed077de29b4bfacae4
Andrew Person <professor_smart@hotmail.com>
2021-10-23 12:06:33 +1100

    Revert "Updated dependencies and fixed bug with getting incorrect next dailytimetable"
    This reverts commit 17e1514e8fda1e4bea90e7cd8e5a3fe9e250a4d4.

--- functions/resources.js
+++ functions/resources.js
@@ -39,45 +39,28 @@ async function resources(payload) {
 
     var promises = [];
 
-    var nextDailyTimetablePromise;
-
     Object.keys(RESOURCES).forEach(resource => {
         promises.push(getResource(resource, token).then(resourceResponse => {
             result.result[RESOURCES[resource]] = resourceResponse;
-            
-            if (resource == "timetable/daytimetable.json") {
-                var dateString = resourceResponse.date;
-
-                var [year, month, day] = dateString.split("-");
-
-                year = parseInt(year);
-                //Month is 0-indexed for date object
-                month = parseInt(month) - 1;
-                day = parseInt(day);
-
-                var date = new Date(year, month, day);
-
-                date.setDate(date.getDate() + 1);
-
-                year = date.getFullYear();
-                //Month is 0-indexed for date object
-                month = date.getMonth() + 1;
-                day = date.getDate();
-
-                if (month < 10) month = `0${month}`;
-                if (day < 10) day = `0${day}`;
-
-                dateString = `${year}-${month}-${day}`;
-
-                nextDailyTimetablePromise = getResource(`timetable/daytimetable.json?date=${dateString}`, token).then(nextDailyTimetable => {
-                    result.result["next-dailytimetable"] = nextDailyTimetable;
-                });
-            }
         }));
     });
 
+    var now = new Date();
+    var year = (now.getFullYear()).toString();
+    var month = (now.getMonth() + 1).toString();
+    var day = (now.getDate() + 1).toString();
+
+    if (month < 10)
+        month = `0${month}`;
+
+    if (day < 10)
+        day = `0${day}`;
+
+    promises.push(getResource(`timetable/daytimetable.json?date=${year}-${month}-${day}`, token).then(resourceResponse => {
+        result.result["next-dailytimetable"] = resourceResponse;
+    }));
+
     await Promise.all(promises);
-    await nextDailyTimetablePromise;
 
     return {
         statusCode: 200,
