38ae8410c55c98f17d4a9e5349998bd1c09f78eb
Andrew P <professor_smart@hotmail.com>
2022-01-16 22:49:12 +0000

    Added error reporting

--- functions/error.ts
+++ functions/error.ts
@@ -1,11 +1,29 @@
-import axios from "axios";
 import { create } from "../lib/function";
-import { Token } from "../lib/token";
+
+import { createConnection } from "mysql";
 
 create(async (payload: any) => {
-    console.log(payload);
+    let connection = createConnection({
+        host: payload.sql_host,
+        user: payload.sql_user,
+        password: payload.sql_password
+    });
+
+    await new Promise<void>((resolve, reject) => {
+        connection.connect(err => {
+            if (err) reject(err);
+            else resolve();
+        });
+    });
+
+    await new Promise<void>((resolve, reject) => {
+        connection.query("INSERT INTO incidents (error_name, stack_trace, version) VALUES (?, ?, ?)", [], err => {
+            if (err) reject(err);
+            else resolve();
+        });
+    });
 
     return {
-        body: payload
+        body: "Success"
     };
 });
