8c64a47224ea0ba2cb539c6a198e777814903e6c
Andrew P <professor_smart@hotmail.com>
2022-01-17 01:01:44 +0000

    Changed back to mysql

--- functions/error.ts
+++ functions/error.ts
@@ -1,15 +1,29 @@
 import { create } from "../lib/function";
 
-import { createConnection } from "promise-mysql";
+import { createConnection } from "mysql";
 
 create(async (payload: any) => {
-    let connection = await createConnection({
+    let connection = createConnection({
         host: payload.SQL_HOST,
         user: payload.SQL_USER,
         password: payload.SQL_PASSWORD
     });
 
-    await connection.query("INSERT INTO incidents (error_name, stack_trace, version) VALUES (?, ?, ?)", [payload.error_name, payload.stack_trace, payload.version]);
+    await new Promise<void>((resolve, reject) => {
+        connection.connect(err => {
+            if (err) reject(err);
+            else resolve();
+        })
+    });
+
+    await new Promise<void>((resolve, reject) => {
+        connection.query("INSERT INTO incidents (error_name, stack_trace, version) VALUES (?, ?, ?)",
+                         [payload.error_name, payload.stack_trace, payload.version],
+                         err => {
+                             if (err) reject(err);
+                             else resolve();
+                         });
+    });
 
     return {
         body: "Success"
